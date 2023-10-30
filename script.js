46b34058a64087342eadd9f032d500dd858c1e48
Mohamed Ndayishimiye <ndayimodi@gmail.com>
2023-10-30 15:44:07 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,31 @@
+// let information = document.getElementById('content');
+// console.log(information);
+
+// const nez = document.querySelector('#content');
+// console.log(nez);
+// document.title= "Modifying the dom";
+// nez.style.color= 'red';
+// console.log(document.title)
+
+// function changeBackgroundColor() {
+//     document.body.style.backgroundColor = "rgb(240, 240, 240)";
+// }
+//  changeBackgroundColor();
+
+//  array.forEach(element => {
+    
+//  });
+
+// const bodyElement = document.querySelector('body');
+// const children = bodyElement.children
+// for (const child of children) {
+//     console.log(child);
+    
+// }
+
+const secondSection = document.querySelector('#content');
+const children = secondSection.children
+for (const child of children) {
+    console.log(child);
+    
+}
