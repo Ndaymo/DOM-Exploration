078922236a2040dfd481a2156703507694f4a329
Mohamed Ndayishimiye <ndayimodi@gmail.com>
2023-10-30 16:19:56 +0100

    Select all elements .important

---
+++ Selectors/script.js
@@ -0,0 +1,9 @@
+// const importantElements = document.querySelectorAll('.important');
+// for (const element of importantElements) {
+//     element.title = 'This is an important item';
+// }
+const importantElements = document.querySelectorAll('.important')
+for (const element of importantElements) {
+    console.log(element);
+    
+}
