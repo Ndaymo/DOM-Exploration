a6144f8c79d1af945f3840de48f543146c3bb545
Mohamed Ndayishimiye <ndayimodi@gmail.com>
2023-10-31 09:42:33 +0100

    Dissapear image that don't contain .important

--- Selectors/script.js
+++ Selectors/script.js
@@ -2,8 +2,27 @@
 // for (const element of importantElements) {
 //     element.title = 'This is an important item';
 // }
-const importantElements = document.querySelectorAll('.important')
-for (const element of importantElements) {
-    console.log(element);
-    element.title= 'This is an important item';
+
+// ------------Give the .important elements a brand new title ---------
+
+// const importantElements = document.querySelectorAll('.important')
+// for (const element of importantElements) {
+//     console.log(element);
+//     element.title= 'This is an important item';
+// }
+
+// select  all img
+
+// const everyPicture = document.querySelectorAll("img");
+// for (const picture of everyPicture) {
+//    console.log(picture); 
+
+// }
+
+const images = document.querySelectorAll('img');
+for (const image of images) {
+    if(!image.classList.contains('important')){
+        image.style.display = 'none';
+    }
+    
 }
