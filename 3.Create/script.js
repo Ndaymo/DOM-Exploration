7414f82b3dc121e7308c338980b1b0df6dc92ae4
Mohamed Ndayishimiye <ndayimodi@gmail.com>
2023-10-31 15:53:52 +0100

    Change order with every update

--- 3.Create/script.js
+++ 3.Create/script.js
@@ -1,31 +1,85 @@
-const learners = ["Lucas", "Kilian", "Alec", "Mohamed", "Pieter", "Rebecca", "Alessandro", "Josué", 
-"Thibault", "Eduarda", "Mohammed", "Funda", "Luis", "Alexandru", "Andrej", "Danté", "Jonasi", "Kelsey", 
-"Rana", "Jana", "Sieglinde", "Gustave", "Sezin", "Thierry", "Sylvie", "Anaïs"];
+// const learners = ["Lucas", "Kilian", "Alec", "Mohamed", "Pieter", "Rebecca", "Alessandro", 
+// "Josué", "Thibault", "Eduarda", "Mohammed", "Funda", "Luis", "Alexandru", "Andrej", "Danté", "Jonasi", 
+// "Kelsey", "Rana", "Jana", "Sieglinde", "Gustave", "Sezin", "Thierry", "Sylvie", "Anaïs"];
+
+// const articleElement = document.querySelector('article');
+
+// for (const learner of learners) {
+//   // Create a new section element
+//   const sectionElement = document.createElement('section');
+
+//   // Generate a random background color
+//   const backgroundColor = randomColor();
+
+//   // Set the background color of the section element
+//   sectionElement.style.backgroundColor = backgroundColor;
+
+//   // Create a paragraph element
+//   const paragraphElement = document.createElement('p');
+
+//   // Set the text content of the paragraph element to the learner's name
+//   paragraphElement.textContent = learner;
+
+//   // Append the paragraph element to the section element
+//   sectionElement.appendChild(paragraphElement);
+
+//   // Append the section element to the article element
+//   articleElement.appendChild(sectionElement);
+// }
+
+// function randomColor() {
+//   // Generate a random hex code
+//   const hexCode = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+
+//   return hexCode;
+// }
+
+
+// const allLearners = ["Haji", "Hussein", "Rajabu"];
+// const articleEle = document.querySelector('article');
+
+// for (const learnerx of allLearners) {
+//     const nSection = document.createElement('section');
+//     const backgroundColor = randomColor();
+//     const nParagraph = document.createElement('p');
+//     nParagraph = document.createTextNode(learnerx);
+//     nParagraph.appendChild(nSection);
+    
+//     nSection.appendChild(articleEle);
+
+    
+// }
+
+const learners = ["Lucas", "Kilian", "Alec", "Mohamed", "Pieter", "Rebecca", "Alessandro", "Josué", "Thibault", "Eduarda", "Mohammed", "Funda", "Luis", "Alexandru", "Andrej", "Danté", "Jonasi", "Kelsey", "Rana", "Jana", "Sieglinde", "Gustave", "Sezin", "Thierry", "Sylvie", "Anaïs"];
 
 const articleElement = document.querySelector('article');
 
+// Shuffle the learners array
+learners.sort(() => Math.random() - 0.5);
+
+// Append the learners to the article element in the shuffled order
 for (const learner of learners) {
-// Create a new section element
-const sectionElement = document.createElement('section');
+  // Create a new section element
+  const sectionElement = document.createElement('section');
 
-//Generate a random background color
-const backgroundColor = randomColor();
+  // Generate a random background color
+  const backgroundColor = randomColor();
 
-// Set the background color of the section element
-sectionElement.style.backgroundColor = backgroundColor;
+  // Set the background color of the section element
+  sectionElement.style.backgroundColor = backgroundColor;
 
-// Create a paragraph element
-const paragraphElement = document.createElement('p');
+  // Create a paragraph element
+  const paragraphElement = document.createElement('p');
 
-// Set the text content of the paragraph element to the learner's name
-paragraphElement.textContent = learner;
+  // Set the text content of the paragraph element to the learner's name
+  paragraphElement.textContent = learner;
 
-// Append the paragraph element to the section element
-sectionElement.appendChild(paragraphElement);
+  // Append the paragraph element to the section element
+  sectionElement.appendChild(paragraphElement);
 
-// Append the section element to the article element
-articleElement.appendChild(sectionElement);
-// }
+  // Append the section element to the article element
+  articleElement.appendChild(sectionElement);
+}
 
 function randomColor() {
   // Generate a random hex code
