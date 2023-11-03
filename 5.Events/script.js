683ece556fe1c69adb4428eecd11fcc89ac183e5
Mohamed Ndayishimiye <ndayimodi@gmail.com>
2023-11-03 10:20:20 +0100

    Create new corresponding color squares for click event

--- 5.Events/script.js
+++ 5.Events/script.js
@@ -13,4 +13,27 @@ const actionSquares = document.querySelectorAll('.actionsquare')
 for (let actionSquare of actionSquares) {
   actionSquare.addEventListener('click', clickOnSquare)
 }
+//
+
+
+
+// Get the action square elements
+const actionSquareElements = document.querySelectorAll('.actionsquare');
+
+// Add a click event listener to each action square element
+actionSquareElements.forEach((actionSquareElement) => {
+  actionSquareElement.addEventListener('click', (event) => {
+    // Create a new displayed square element
+    const displayedSquareElement = document.createElement('div');
+    displayedSquareElement.classList.add('displayedsquare');
+
+    // Set the color of the displayed square element to the same color as the clicked action square element
+    displayedSquareElement.style.backgroundColor = getComputedStyle(actionSquareElement).backgroundColor;
+
+    // Append the displayed square element to the displayedsquare-wrapper section
+    const displayedSquareWrapperElement = document.querySelector('.displayedsquare-wrapper');
+    displayedSquareWrapperElement.appendChild(displayedSquareElement);
+  });
+});
+
 
